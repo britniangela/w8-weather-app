af85e47493f7d5149bfc8a3879f5da664a6347e7
britniangela <66388266+britniangela@users.noreply.github.com>
2024-07-04 17:36:16 -0400

    added search function with api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+function updateTemperatureValue(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-searched");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "58f3tbaa2acbfcb0981e0ecb4ba2453o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(updateTemperatureValue);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city-searched");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Orlando");
 
 function displayForecast(day) {
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
