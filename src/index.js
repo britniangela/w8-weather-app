4365df45d10179c69b3272c256d8e9990428c702
britniangela <66388266+britniangela@users.noreply.github.com>
2024-07-14 10:19:19 -0400

    updated weather/forecast js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,46 @@ function updateTemperatureValue(response) {
   let temperatureElement = document.querySelector("#temperature-value");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city-searched");
+  let currentDayTimeElement = document.querySelector("#app-day-time");
+  let weatherDescriptionElement = document.querySelector(
+    "#weather-description"
+  );
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let date = new Date(response.data.time * 1000);
+  let temperatureIconElement = document.querySelector("#temperature-icon");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  currentDayTimeElement.innerHTML = formatDate(date);
+  weatherDescriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)}mph`;
+  temperatureIconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temperature-icon" />`;
 
   getForecast(response.data.city);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -49,19 +84,19 @@ function displayForecast(response) {
       forecastHtml =
         forecastHtml +
         `
-      <div class="forecast">
-            <div class="forecast-day">${formatDay(day.time)}</div>
-           <img src="${day.condition.icon_url}" class="forecast-icon">
-            <div class="forecast-temperatures">
-              <div class="forecast-temperature"><strong>${Math.round(
-                day.temperature.maximum
-              )}°</strong></div>
-              <div class="vertical-bar">|</div>
-              <div class="forecast-temperature">${Math.round(
-                day.temperature.minimum
-              )}°</div>
-            </div>
-          </div>`;
+          <div class="forecast">
+                <div class="forecast-day">${formatDay(day.time)}</div>
+               <img src="${day.condition.icon_url}" class="forecast-icon">
+                <div class="forecast-temperatures">
+                  <div class="forecast-temperature"><strong>${Math.round(
+                    day.temperature.maximum
+                  )}°</strong></div>
+                  <div class="vertical-bar">|</div>
+                  <div class="forecast-temperature">${Math.round(
+                    day.temperature.minimum
+                  )}°</div>
+                </div>
+              </div>`;
 
       let forecastElement = document.querySelector("#forecast-container");
       forecastElement.innerHTML = forecastHtml;
